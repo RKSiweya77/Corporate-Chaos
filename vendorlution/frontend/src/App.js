523f37852d95d3227ebc71bbb8febba86bb522a9
sdu4358 <sduduzobuthelezi09@gmail.com>
2025-09-11 19:20:21 +0200

    Standardizing header

--- vendorlution/frontend/src/App.js
+++ vendorlution/frontend/src/App.js
@@ -1,29 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a className="navbar-brand" href="#">Vendorlution</a>
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">Categories</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
+    <Header/>
     <main className='mt-4'>
       <div className='container'>
         {/*Latest Products*/}
