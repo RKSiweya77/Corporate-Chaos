9edb9dd131ddb472d59f1c885df4f62745d65c59
Blesscodez <aphiwemntambo945@gmail.com>
2025-09-15 13:22:39 +0200

    nothing

--- vendorlution/backend_api/backend_api/settings.py
+++ vendorlution/backend_api/backend_api/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'vendorlution',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': 'Phraser18$',
         'HOST': 'localhost',
 
     }
